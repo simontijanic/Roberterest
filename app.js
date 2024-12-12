2891b14cbd6d71b202560d0fce6127c82568718d
simontijanic <simontijanicerkul@gmail.com>
2024-12-12 11:01:25 +0100

    env

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const defaultRoute = require('./routes/defaultRoute');
 const uploadRoute = require('./routes/uploadRoute');
 const databaseController = require('./controllers/databaseController');
