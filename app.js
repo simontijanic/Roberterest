eb4832cbd2f855bd1f0fc2b37535fd817674e5d0
simontijanic <simontijanicerkul@gmail.com>
2024-12-16 09:34:50 +0100

    routes

--- app.js
+++ app.js
@@ -12,21 +12,19 @@ const uploadRoute = require('./routes/uploadRoute');
 const databaseController = require('./controllers/databaseController');
 const limiter = require("./middlewares/limiter")
 
-// Use Helmet for security headers
-app.use(helmet());
+// Static files
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Limit body size to prevent abuse
+const uploadsDir = path.join(__dirname, 'images', 'uploads'); 
+app.use('/images/uploads', express.static(uploadsDir));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json({ limit: '10mb' }));
 
-// Static files
-const uploadsDir = path.join(__dirname, 'images', 'uploads'); // Adjust path as per your new structure
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images/uploads', express.static(uploadsDir));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(helmet());
 
 app.use(
   session({
@@ -41,8 +39,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/', defaultRoute, limiter.generalLimiter);
-app.use('/', uploadRoute, limiter.creationToolLimiter);
+app.use('/', limiter.generalLimiter, defaultRoute);
+app.use('/', limiter.creationToolLimiter, uploadRoute);
 
 // Start server
 app.listen(port, () => {
